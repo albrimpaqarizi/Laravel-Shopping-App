26e73fd017f490743ed31fe307af9473bf7d0456
Edon Hoxha <eh45542@ubt-uni.net>
2020-07-23 22:42:17 +0200

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -13,11 +13,15 @@
                         {{ session('status') }}
                     </div>
                     @endif
-
+                    @if(Auth::check())
                     {{ __('You are logged in!') }}
+                    @endif
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+    <h3>Welcome to Laravel E-Commerce</h3>
+    <img src="storage/images/wallpaper.png" alt="img" style="width:100%; height:auto;">
+    <a href="/" style="display:flex; justify-content:center;"><button class="btn btn-primary" style="width:50%; ">Shop Now</button></a>
 </div>
 @endsection
